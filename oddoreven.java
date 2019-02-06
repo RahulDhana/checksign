76c458e315946e4394e5cdf197c15e8e284a795e
RahulDhana <47377695+RahulDhana@users.noreply.github.com>
2019-02-06 12:54:28 +0530

    Add files via upload

---
+++ oddoreven.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class oddoreven{
+public static void main(String[] args){
+int a;
+Scanner scan=new Scanner(System.in);
+System.out.print("enter the numer:");
+a=scan.nextInt();
+if(a%2==0){
+System.out.print("number is even");
+}
+else{
+System.out.print("number is odd");
+}
+}
+}
+
